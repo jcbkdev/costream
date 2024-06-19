f3d1f25d9707610dca0bcad48a246e2972db6941
jcbkdev <kubkon134@gmail.com>
2024-06-19 14:15:13 +0200

    removed comment

--- src/elements/costreamButton.ts
+++ src/elements/costreamButton.ts
@@ -13,19 +13,4 @@ export function addCostreamButton(){
     headerRightElement.appendChild(costreamButton);
     costreamButton.addEventListener("click", addCostreamSection);
     console.log("ADDED BUTTON")
-
-    /* 
-    bg var(--color-background-button-primary-default)
-    text var(--color-text-button-primary)
-    font weight bold
-    border radius var(--border-radius-medium)
-    padding 5px 10px
-    margin-left 1rem
-
-    hover:
-    bg var(--color-background-button-brand-hover)
-
-    active:
-    bg var(--color-background-button-brand-active)
-    */
 }
